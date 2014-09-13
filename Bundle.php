ed92d7506cb7332864bffe45b51d345c91b17276
Michel Hognerud <leyou.m@gmail.com>
2014-09-13 13:04:57 +0200

    update bundle

--- Bundle.php
+++ Bundle.php
@@ -2,7 +2,7 @@
 namespace Asgard\Jsonentities;
 
 class Bundle extends \Asgard\Core\BundleLoader {
-	public function run($container) {
+	public function run(\Asgard\Container\Container $container) {
 		$container['httpKernel']->filterAll('Asgard\Jsonentities\Filter');
 	}
 }
